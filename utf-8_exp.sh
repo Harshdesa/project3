485d2dd97fa5511d965fda7693b3417fe2f56196
HARSH DESAI <harshdesai@HARSHs-iMac.lan>
2020-06-30 14:56:54 -0500

    Initial commit

---
+++ utf-8_exp.sh
@@ -0,0 +1,26 @@
+echo 'Character 00000: \u0000' | ascii2uni -a U -q
+echo 'Character 00001: \u0001' | ascii2uni -a U -q
+echo 'Character 00002: \u0002' | ascii2uni -a U -q
+echo 'Character 00003: \u0003' | ascii2uni -a U -q
+echo 'Character 00004: \u0004' | ascii2uni -a U -q
+echo 'Character 00005: \u0005' | ascii2uni -a U -q
+echo 'Character 00006: \u0006' | ascii2uni -a U -q
+echo 'Character 00007: \u0007' | ascii2uni -a U -q
+echo 'Character 00008: \u0008' | ascii2uni -a U -q
+echo 'Character 00009: \u0009' | ascii2uni -a U -q
+echo 'Character 0000A: \u000A' | ascii2uni -a U -q
+echo 'Character 0000B: \u000B' | ascii2uni -a U -q
+echo 'Character 0000C: \u000C' | ascii2uni -a U -q
+echo 'Character 0000D: \u000D' | ascii2uni -a U -q
+echo 'Character 0000E: \u000E' | ascii2uni -a U -q
+echo 'Character 0000F: \u000F' | ascii2uni -a U -q
+echo 'Character 00010: \u0010' | ascii2uni -a U -q
+
+#Symbols and Numbers
+echo 'Character 00020: \u0020' | ascii2uni -a U -q
+echo 'Character 00021: \u0021' | ascii2uni -a U -q
+echo 'Character 00040: \u0040' | ascii2uni -a U -q
+
+#Capital Letters
+echo 'Character 00041: \u0041' | ascii2uni -a U -q
+echo 'Character 0005A: \u005A' | ascii2uni -a U -q
